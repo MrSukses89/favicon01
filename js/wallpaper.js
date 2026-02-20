c48f950611228052ba343ecf5299a454455cf3e4
MrSukses89 <andi8evo@gmail.com>
2026-02-20 08:20:12 +0800

    update wallpaper

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -10,6 +10,10 @@ const koleksiWallpaper = [
     "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?auto=format&fit=crop&w=1920",
     "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1920",
     "https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?auto=format&fit=crop&w=1920",
+    "wallpaper/4.jpg",
+    "wallpaper/3.jpg"
+   
+   
 ];
 
 let currentIndex = 0;
@@ -26,11 +30,11 @@ function nextWallpaper() {
     console.log("Wallpaper: " + targetWall);
 }
 
-// 3. Fungsi Auto (2 Menit)
+// 3. Fungsi Auto (10 Menit)
 function toggleAutoWallpaper() {
     const isChecked = document.getElementById('autoWallpaperCheck').checked;
     if (isChecked) {
-        autoInterval = setInterval(nextWallpaper, 120000);
+        autoInterval = setInterval(nextWallpaper, 600000); // 10 menit = 600000 milidetik
         console.log("Auto ON");
     } else {
         clearInterval(autoInterval);
